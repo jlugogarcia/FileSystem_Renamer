ec55a645bbc8ef6a18111122e1a3100e6c79a9b0
José Alejandro Lugo-García <jalugogarcia@gmail.com>
2022-10-24 10:14:41 -0400

    Add project files.

---
+++ FileSystem_Renamer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parent = new System.IO.DirectoryInfo(@"Parent_Folder\To_Rename_Content");
+
+            foreach (var child in parent.GetDirectories())
+            {
+                var newName = child.FullName.Replace("StringToReplace", "NewString");
+
+                if (newName == child.FullName)
+                {
+                    continue;
+                }
+                child.MoveTo(newName);
+            }
+        }
+    }
+}
